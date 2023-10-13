8519d22136f23531e239a6c6fc8b6bc68dbd648c
Jake <jacob.m.livingston@gmail.com>
2023-10-13 11:00:47 -0700

    All current work on post jam

--- Assets/Scripts/AutoNarrativeItem.cs
+++ Assets/Scripts/AutoNarrativeItem.cs
@@ -1,21 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using static System.Globalization.CultureInfo;
 
 public class AutoNarrativeItem : MonoBehaviour {
 
-    public  string                            id;
-    public  int                               number;
-    public  Day                               day;
-    public  Character                         character;
-    public  bool                              phone;
-    public  int                               offset;
-    public  List<AudioClip>                   sounds;
     private Dictionary<string, NarrationItem> _narrationItems = new Dictionary<string, NarrationItem>();
     public CharacterList characterList;
     
@@ -52,141 +41,149 @@ public class AutoNarrativeItem : MonoBehaviour {
     
     
     public void LoadDataFromCSV() {
-        //
-        // if (csvFile == null) {
-        //     Debug.LogError("CSV file reference missing!");
-        //     return;
-        // }
-        //
-        // string[] lines = csvFile.text.Split('\n');
-        //
-        // int startIndex = 0;
-        //
-        //
-        // for (int i = 1; i < lines.Length; i++) {
-        //     string line = lines[i].Trim();
-        //     if (string.IsNullOrEmpty(line)) {
-        //         continue;
-        //     }
-        //     
-        //     string[] fields = line.Split("\t");
-        //     try {
-        //         if(fields.Length<1) continue;
-        //         NarrationItem instance = ScriptableObject.CreateInstance<NarrationItem>();
-        //         // Debug.Log(String.Join(",", fields.ToList()));
-        //         instance.id = fields[0];
-        //         instance.line = fields[1];
-        //         switch (fields[2]) {
-        //             case "Day 1":
-        //                 instance.day = Day.One;
-        //                 break;
-        //             case "Day 2":
-        //                 instance.day = Day.Two;
-        //                 break;
-        //             case "Day 3":
-        //                 instance.day = Day.Three;
-        //                 break;
-        //         }
-        //         if (fields[3].Equals("???")) {
-        //             instance.unknownCharacter = true ;
-        //         }
-        //         else if (fields[3].Equals("")){
-        //             // instance.character.character = CharacterEnum.None;
-        //
-        //         }
-        //         else {
-        //             instance.character = characterList.characters.Find(c=> {
-        //                 return c.character.Equals((CharacterEnum)Enum.Parse(typeof(CharacterEnum), fields[3]));
-        //             });
-        //         }
-        //
-        //         instance.characterArt1 = Art.NA;
-        //         instance.characterArt2 = Art.NA;
-        //         instance.characterArt3 = Art.NA;
-        //
-        //         if (fields.Length>4 && !fields[4].Equals("N/A") && fields[4].Trim() !="") {
-        //             instance.characterArt1 = (Art)Enum.Parse(typeof(Art), fields[4]);
-        //         }
-        //         if (fields.Length>5 && !fields[5].Equals("N/A") && fields[5].Trim() !="") {
-        //             instance.characterArt2 = (Art)Enum.Parse(typeof(Art), fields[5]);
-        //         }
-        //         if (fields.Length>6 && !fields[6].Equals("N/A") && fields[6].Trim() !="" ) {
-        //             instance.characterArt3 = (Art)Enum.Parse(typeof(Art), fields[6]);
-        //         }
-        //         
-        //         
-        //         if(fields.Length>7 && fields[7] != "" ) {
-        //             instance.dialogueType = (DialogueType)Enum.Parse(typeof(DialogueType), fields[7]);
-        //         }            
-        //         if(fields.Length>8 && fields[8] != "") {
-        //             instance.background = (Art)Enum.Parse(typeof(Art), fields[8]);
-        //         }            
-        //         if(fields.Length>13 && !fields[13].Equals("")) {
-        //             instance.music = (Sounds)Enum.Parse(typeof(Sounds), fields[13]);
-        //         }
-        //         else {
-        //             instance.music = Sounds.None;
-        //         }
-        //
-        //         instance.sounds = new List<Sounds>();
-        //         if (fields.Length>14 && fields[14] != "" && fields[14] != "N/A") {
-        //             instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[14]));
-        //         }
-        //         if (fields.Length>15 && fields[15] != "" && fields[15] != "N/A") {
-        //             instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[15]));
-        //         }
-        //         if (fields.Length>16 && fields[16] != "" && fields[16] != "N/A") {
-        //             instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[16]));
-        //         }
-        //         if (fields.Length>17 && fields[17] != "" && fields[17] != "N/A") {
-        //             instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[17]));
-        //         }
-        //
-        //         if (fields.Length > 18 && fields[18] != "") {
-        //             instance.ambience = (Sounds)Enum.Parse(typeof(Sounds), fields[18]);
-        //
-        //         }else {
-        //             instance.ambience = Sounds.None;
-        //         }
-        //
-        //         
-        //         _narrationItems[fields[0]] = instance;
-        //         EditorUtility.SetDirty(instance);
-        //         AssetDatabase.CreateAsset(instance, "Assets/Narrative/" + instance.id + ".asset");
-        //     }
-        //     catch (Exception e) {
-        //         Debug.Log($"Failed on {line}");
-        //         Debug.LogWarning(e);
-        //     }
-        // }
-        // //
-        // for (int i = 1; i < lines.Length; i++) {
-        //     string line = lines[i].Trim();
-        //     if (string.IsNullOrEmpty(line)) {
-        //         continue;
-        //     }
-        //     string[] fields = line.Split('\t');
-        //
-        //     NarrationItem narrationItem = _narrationItems[fields[0]];
-        //     string firstChoice = fields.Length > 10 ? fields[10] : "";
-        //     if (fields.Length>9 && _narrationItems.ContainsKey(fields[9])) {
-        //         narrationItem.next1 = new NextNarrative(_narrationItems[fields[9]],firstChoice);
-        //
-        //     }
-        //     if (fields.Length>11 && fields[11] != "" &&_narrationItems.ContainsKey(fields[11]) ) {
-        //         narrationItem.next2 = new NextNarrative(_narrationItems[fields[11]],fields[12]);
-        //         switch (narrationItem.day) {
-        //             case Day.One:
-        //                 break;
-        //             case Day.Two:
-        //                 break;
-        //             case Day.Three:
-        //                 break;
-        //         }
-        //     }
-            
-        // } 
         
+        if (csvFile == null) {
+            Debug.LogError("CSV file reference missing!");
+            return;
+        }
+        
+        string[] lines = csvFile.text.Split('\n');
+        
+        int startIndex = 0;
+        
+        
+        for (int i = 1; i < lines.Length; i++) {
+            string line = lines[i].Trim();
+            if (string.IsNullOrEmpty(line)) {
+                continue;
+            }
+            
+            string[] fields = line.Split("\t");
+            try {
+                if(fields.Length<1) continue;
+                NarrationItem instance = ScriptableObject.CreateInstance<NarrationItem>();
+                // Debug.Log(String.Join(",", fields.ToList()));
+                instance.id = fields[0];
+                instance.line = fields[1];
+                switch (fields[2]) {
+                    case "Day 1":
+                        instance.day = Day.One;
+                        break;
+                    case "Day 2":
+                        instance.day = Day.Two;
+                        break;
+                    case "Day 3":
+                        instance.day = Day.Three;
+                        break;
+                }
+                if (fields[3].Equals("???")) {
+                    instance.unknownCharacter = true ;
+                }
+                else if (fields[3].Equals("")){
+                    // instance.character.character = CharacterEnum.None;
+        
+                }
+                else {
+                    instance.character = characterList.characters.Find(c=> {
+                        return c.character.Equals((CharacterEnum)Enum.Parse(typeof(CharacterEnum), fields[3]));
+                    });
+                }
+        
+                instance.characterArt1 = Art.NA;
+                instance.characterArt2 = Art.NA;
+                instance.characterArt3 = Art.NA;
+        
+                if (fields.Length>4 && !fields[4].Equals("N/A") && fields[4].Trim() !="") {
+                    instance.characterArt1 = (Art)Enum.Parse(typeof(Art), fields[4]);
+                }
+                if (fields.Length>5 && !fields[5].Equals("N/A") && fields[5].Trim() !="") {
+                    instance.characterArt2 = (Art)Enum.Parse(typeof(Art), fields[5]);
+                }
+                if (fields.Length>6 && !fields[6].Equals("N/A") && fields[6].Trim() !="" ) {
+                    instance.characterArt3 = (Art)Enum.Parse(typeof(Art), fields[6]);
+                }
+                
+                
+                if(fields.Length>7 && fields[7] != "" ) {
+                    instance.dialogueType = (DialogueType)Enum.Parse(typeof(DialogueType), fields[7]);
+                }            
+                if(fields.Length>8 && fields[8] != "") {
+                    instance.background = (Art)Enum.Parse(typeof(Art), fields[8]);
+                }            
+                if(fields.Length>13 && !fields[13].Equals("")) {
+                    instance.music = (Sounds)Enum.Parse(typeof(Sounds), fields[13]);
+                }
+                else {
+                    instance.music = Sounds.None;
+                }
+        
+                instance.sounds = new List<Sounds>();
+                if (fields.Length>14 && fields[14] != "" && fields[14] != "N/A") {
+                    instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[14]));
+                }
+                if (fields.Length>15 && fields[15] != "" && fields[15] != "N/A") {
+                    instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[15]));
+                }
+                if (fields.Length>16 && fields[16] != "" && fields[16] != "N/A") {
+                    instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[16]));
+                }
+                if (fields.Length>17 && fields[17] != "" && fields[17] != "N/A") {
+                    instance.sounds.Add((Sounds)Enum.Parse(typeof(Sounds), fields[17]));
+                }
+        
+                if (fields.Length > 18 && fields[18] != "") {
+                    instance.ambience = (Sounds)Enum.Parse(typeof(Sounds), fields[18]);
+        
+                }else {
+                    instance.ambience = Sounds.None;
+                }
+        
+                
+                _narrationItems[fields[0]] = instance;
+                
+                EditorUtility.SetDirty(instance);
+            }
+            catch (Exception e) {
+                Debug.Log($"Failed on {line}");
+                Debug.LogWarning(e);
+            }
+        }
+        //
+        for (int i = 1; i < lines.Length; i++) {
+            string line = lines[i].Trim();
+            if (string.IsNullOrEmpty(line)) {
+                continue;
+            }
+            string[] fields = line.Split('\t');
+        
+            NarrationItem narrationItem = _narrationItems[fields[0]];
+            string firstChoice = fields.Length > 10 ? fields[10] : "";
+            if (fields.Length>9 && _narrationItems.ContainsKey(fields[9])) {
+                narrationItem.next1 = new NextNarrative(_narrationItems[fields[9]],firstChoice);
+        
+            }
+            if (fields.Length>11 && fields[11] != "" &&_narrationItems.ContainsKey(fields[11]) ) {
+                narrationItem.next2 = new NextNarrative(_narrationItems[fields[11]],fields[12]);
+                switch (narrationItem.day) {
+                    case Day.One:
+                        break;
+                    case Day.Two:
+                        break;
+                    case Day.Three:
+                        break;
+                }
+            }
+
+            
+        }
+        foreach (NarrationItem narrationItem in _narrationItems.Values) {
+            AssetDatabase.CreateAsset(narrationItem, "Assets/Narrative/" + narrationItem.id + ".asset");
+
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
     }
     // [D2D-1]	[ROD2-1]		FALSE	FALSE	TRUE				I jab my fingers against the screen of my phone the little avatar hops up and down in an effort to dodge the man-eating kale. 
 }
--- Assets/Scripts/ControlBackgroundMusic.cs
+++ Assets/Scripts/ControlBackgroundMusic.cs
@@ -8,29 +8,33 @@ public class ControlBackgroundMusic : MonoBehaviour {
     public        AudioSource            musicSource;
     public        AudioSource            ambientSource;
 
-    public List<Sound> ambientSounds;
-    public List<Sound> music;
+    public  List<Sound> ambientSounds;
+    public  List<Sound> music;
+    private float       currentVolume;
+    private float       targetVolume;
+    public  float       transitionDuration = 2.0f; // Editable transition duration in seconds
 
     private void Awake() {
         if (instance == null) {
             instance = this;
         }
+
+        currentVolume = musicSource.volume;
     }
 
     public void ChangeSong(Sounds song) {
         if(song.Equals(Sounds.None)) {
-            musicSource.Stop();
+            StartCoroutine(FadeOutMusic());
             musicSource.clip = null;
             return;
         }
         Sound sound = music.Find(m=>m.sound.Equals(song));
         if( sound == null) return;
         if (musicSource.clip == null || (musicSource.clip != null && !musicSource.clip.name.Equals(sound.audioClip.name))) {
-            ChangeSound(sound.audioClip,musicSource);
+            StartCoroutine(CrossfadeMusic(sound.audioClip));
+            // ChangeSound(sound.audioClip,musicSource);
 
         }
-
-        
     }
     
     public void ChangeAmbient(Sounds ambience) {
@@ -49,6 +53,60 @@ public class ControlBackgroundMusic : MonoBehaviour {
         source.Play();
     }
     
+    private IEnumerator CrossfadeMusic(AudioClip newTrack) {
+        targetVolume = 0.0f;
+
+        // Fade out the current music
+        float startVolume = musicSource.volume;
+        float startTime = Time.time;
+        float elapsedTime = 0;
+
+        while (elapsedTime < transitionDuration) {
+            elapsedTime = Time.time - startTime;
+            musicSource.volume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / transitionDuration);
+            yield return null;
+        }
+
+        musicSource.volume = targetVolume;
+        musicSource.Stop();
+
+        // Change to the new music track
+        musicSource.clip = newTrack;
+        musicSource.Play();
+
+        // Fade in the new music
+        targetVolume = currentVolume;
+        startTime = Time.time;
+        elapsedTime = 0;
+
+        while (elapsedTime < transitionDuration) {
+            elapsedTime = Time.time - startTime;
+            Debug.Log(musicSource.volume);
+
+            musicSource.volume = Mathf.Lerp(targetVolume, musicSource.volume, elapsedTime / transitionDuration);
+            yield return null;
+        }
+
+        musicSource.volume = currentVolume;
+    }
+    
+    private IEnumerator FadeOutMusic()
+    {
+        targetVolume = 0.0f;
+    
+        float startVolume = musicSource.volume;
+        float startTime = Time.time;
+        float elapsedTime = 0;
+
+        while (elapsedTime < transitionDuration) {
+            elapsedTime = Time.time - startTime;
+            musicSource.volume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / transitionDuration);
+            yield return null;
+        }
+
+        musicSource.volume = targetVolume;
+        musicSource.Stop();
+    }
     
 }
 
